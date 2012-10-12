5eb4f02f254f3a04970594f139bae1ba3429ec99
Dave Gynn <dave.gynn@rocketfarmstudios.com>
2012-10-12 11:56:16 -0700

    initial commmit of a test project for junit tests

---
+++ mParticleTest-test/src/com/mparticle/test/HomeActivityTest.java
@@ -0,0 +1,32 @@
+package com.mparticle.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.mparticle.MParticleAPI;
+import com.mparticle.demo.HomeActivity;
+
+public class HomeActivityTest extends ActivityInstrumentationTestCase2<HomeActivity> {
+
+    private HomeActivity mActivity;
+    private MParticleAPI mParticleAPI;
+
+    public HomeActivityTest() {
+        super(HomeActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+      super.setUp();
+
+      setActivityInitialTouchMode(false);
+      mActivity = getActivity();
+      mParticleAPI = MParticleAPI.getInstance(mActivity, "01234567890123456789012345678901", "secret");
+
+    }
+
+    public void testPreConditions() {
+        assertTrue(MParticleAPI.debugMode);
+        assertFalse(mParticleAPI.getOptOut());
+    }
+
+}
---
+++ mParticleTest-test/src/com/mparticle/test/MParticleAPITest.java
@@ -0,0 +1,38 @@
+package com.mparticle.test;
+
+import com.mparticle.MParticleAPI;
+
+import android.test.AndroidTestCase;
+
+public class MParticleAPITest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+      super.setUp();
+    }
+
+    public void testGetInstance() {
+
+        MParticleAPI mParticleAPI1 = MParticleAPI.getInstance(getContext(), "01234567890123456789012345678901", "secret");
+        MParticleAPI mParticleAPI2 = MParticleAPI.getInstance(getContext(), "01234567890123456789012345678901", "secret");
+
+        assertSame(mParticleAPI1, mParticleAPI2);
+
+        MParticleAPI mParticleAPI3 = MParticleAPI.getInstance(getContext(), "99999999999999999999999999999999", "secret");
+
+        assertNotSame(mParticleAPI1, mParticleAPI3);
+    }
+
+    public void testSessionLifecycleEvents() {
+
+    }
+
+    public void testSessionEventLogging() {
+
+    }
+
+    public void testSessionDataLogging() {
+
+    }
+
+}
