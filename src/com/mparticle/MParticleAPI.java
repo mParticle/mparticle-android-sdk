9aedeb292f429c7692a00e80432adb753b5564d6
Dave Gynn <dave.gynn@rocketfarmstudios.com>
2012-10-16 16:36:22 -0700

    improve device data collections and map properties to message keys

--- src/com/mparticle/MParticleAPI.java
+++ src/com/mparticle/MParticleAPI.java
@@ -3,16 +3,22 @@ package com.mparticle;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
-import com.mparticle.MessageManager.MessageKey;
-import com.mparticle.MessageManager.MessageType;
-
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.mparticle.MessageManager.MessageKey;
+import com.mparticle.MessageManager.MessageType;
 
 public class MParticleAPI {
 
@@ -232,25 +238,72 @@ public class MParticleAPI {
     public Map<String, Object> collectDeviceProperties() {
         Map<String, Object> properties = new LinkedHashMap<String, Object>();
 
-        properties.put("mparticle_sdk_version", MParticleAPI.VERSION);
-        properties.put("api_key", this.mApiKey);
+        // TODO: verify this is the correct value for this key
+        properties.put(MessageKey.APPLICATION_KEY, this.mApiKey);
+
+        try {
+            String packageName = mContext.getPackageName();
+            PackageInfo pInfo = mContext.getPackageManager().getPackageInfo(packageName, 0);
+            properties.put(MessageKey.APPLICATION_VERSION, pInfo.versionName);
+		} catch (PackageManager.NameNotFoundException e) {
+	        properties.put(MessageKey.APPLICATION_VERSION, "Unknown");
+		}
+
+        properties.put(MessageKey.MPARTICLE_VERSION, MParticleAPI.VERSION);
+
+        // device IDs
+        properties.put(MessageKey.DEVICE_ID, Settings.Secure.getString(mContext.getContentResolver(),
+                Settings.Secure.ANDROID_ID));
+        // TODO: get network MAC addresses?
+
+        // device/OS properties
+        properties.put(MessageKey.MANUFACTURER, android.os.Build.MANUFACTURER);
+        properties.put(MessageKey.PLATFORM, "Android");
+        properties.put(MessageKey.OS_VERSION, android.os.Build.VERSION.SDK_INT);
+        properties.put(MessageKey.MODEL, android.os.Build.MODEL);
+
+        // TODO: screen height/width
+
+        // locales
+        Locale locale = Locale.getDefault();
+        properties.put(MessageKey.DEVICE_COUNTRY, locale.getDisplayCountry());
+        properties.put(MessageKey.DEVICE_LOCALE_COUNTRY, locale.getCountry());
+        properties.put(MessageKey.DEVICE_LOCALE_LANGUAGE, locale.getLanguage());
+
+        // TODO: network
+        TelephonyManager telephonyManager = (TelephonyManager)mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        properties.put(MessageKey.NETWORK_CARRIER, telephonyManager.getNetworkOperatorName());
+        properties.put(MessageKey.NETWORK_COUNTRY, telephonyManager.getNetworkCountryIso());
+        // TODO: android appears to combine MNC+MCC into network operator
+        properties.put(MessageKey.MOBILE_NETWORK_CODE, telephonyManager.getNetworkOperator());
+        properties.put(MessageKey.MOBILE_COUNTRY_CODE, telephonyManager.getNetworkOperator());
+
+        // additional info available but possibly not needed
+        ConnectivityManager connectivityManager = (ConnectivityManager)mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        properties.put("extra_mobile_network_type", telephonyManager.getNetworkType());
+
+        // NOTE: this requires ACCESS_NETWORK_STATE permission - which should already be granted. possibly move check elsewhere.
+        if (PackageManager.PERMISSION_GRANTED ==
+                mContext.checkCallingOrSelfPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)) {
+	        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+	        properties.put(MessageKey.DATA_CONNECTION, networkInfo.getTypeName());
+        } else {
+            properties.put(MessageKey.DATA_CONNECTION, "Forbidden");
+        }
+
+        properties.put("device", android.os.Build.DEVICE);
+        properties.put("display", android.os.Build.DISPLAY);
+        properties.put("hardware", android.os.Build.HARDWARE);
+        properties.put("build_id", android.os.Build.ID);
+        properties.put("product", android.os.Build.PRODUCT);
+
+        // internal diagnostics for development, to be removed
         properties.put("secret", this.mSecret);
         properties.put("session_timeout", this.mSessionTimeout);
         properties.put("session_id", this.mSessionID);
         properties.put("session_start", this.mSessionStartTime);
         properties.put("last_event", this.mLastEventTime);
         properties.put("session_end", this.mSessionEndTime);
-
-        properties.put("ANDROID_ID", Settings.Secure.getString(mContext.getContentResolver(),
-                Settings.Secure.ANDROID_ID));
-
-        properties.put("manufacturer", android.os.Build.MANUFACTURER);
-        properties.put("device", android.os.Build.DEVICE);
-        properties.put("display", android.os.Build.DISPLAY);
-        properties.put("hardware", android.os.Build.HARDWARE);
-        properties.put("id", android.os.Build.ID);
-        properties.put("model", android.os.Build.MODEL);
-        properties.put("product", android.os.Build.PRODUCT);
 
         return properties;
     }
