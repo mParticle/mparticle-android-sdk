6255dbc1a6467f8431c6acce284f20b2b3327460
Dave Gynn <dave.gynn@rocketfarmstudios.com>
2012-12-12 15:15:17 -0800

    global code reformatting cleanup for consistency

--- src/com/mparticle/MessageManager.java
+++ src/com/mparticle/MessageManager.java
@@ -25,12 +25,14 @@ import com.mparticle.Constants.ConfigKeys;
 import com.mparticle.Constants.MessageKey;
 import com.mparticle.Constants.MessageType;
 
-/* package-private */ class MessageManager {
+/* package-private */class MessageManager {
 
     private static final String TAG = Constants.LOG_TAG;
 
-    private static final HandlerThread sMessageHandlerThread = new HandlerThread("mParticleMessageHandler", Process.THREAD_PRIORITY_BACKGROUND);
-    private static final HandlerThread sUploadHandlerThread = new HandlerThread("mParticleUploadHandler", Process.THREAD_PRIORITY_BACKGROUND);
+    private static final HandlerThread sMessageHandlerThread = new HandlerThread("mParticleMessageHandler",
+            Process.THREAD_PRIORITY_BACKGROUND);
+    private static final HandlerThread sUploadHandlerThread = new HandlerThread("mParticleUploadHandler",
+            Process.THREAD_PRIORITY_BACKGROUND);
 
     private static String sActiveNetworkName = "offline";
     private static Location sLocation;
@@ -48,11 +50,13 @@ import com.mparticle.Constants.MessageType;
         sUploadHandlerThread.start();
     }
 
-    // This constructor is needed to enable mocking with Mockito and Dexmaker and should never be called
-    /* package-private */ MessageManager() { throw new UnsupportedOperationException(); }
+    // This constructor is needed to enable mocking with Mockito and Dexmaker only
+    /* package-private */MessageManager() {
+        throw new UnsupportedOperationException();
+    }
 
     // Package-private constructor for unit tests only
-    /* package-private */ MessageManager(MessageHandler messageHandler, UploadHandler uploadHandler) {
+    /* package-private */MessageManager(MessageHandler messageHandler, UploadHandler uploadHandler) {
         mMessageHandler = messageHandler;
         mUploadHandler = uploadHandler;
     }
@@ -76,7 +80,7 @@ import com.mparticle.Constants.MessageType;
                 Log.w(TAG, "Failed to configure mParticle with '" + ConfigKeys.ENABLE_SSL + "' setting");
             }
         }
-        if (config.containsKey(ConfigKeys.PROXY_HOST) && config.containsKey(ConfigKeys.PROXY_PORT) ) {
+        if (config.containsKey(ConfigKeys.PROXY_HOST) && config.containsKey(ConfigKeys.PROXY_PORT)) {
             try {
                 this.setConnectionProxy(config.getProperty(ConfigKeys.PROXY_HOST),
                         Integer.parseInt(config.getProperty(ConfigKeys.PROXY_PORT)));
@@ -87,7 +91,8 @@ import com.mparticle.Constants.MessageType;
         }
         if (config.containsKey(ConfigKeys.ENABLE_COMPRESSION)) {
             try {
-                mUploadHandler.setCompressionEnabled(Boolean.parseBoolean(config.getProperty(ConfigKeys.ENABLE_COMPRESSION)));
+                mUploadHandler.setCompressionEnabled(Boolean.parseBoolean(config
+                        .getProperty(ConfigKeys.ENABLE_COMPRESSION)));
             } catch (Throwable t) {
                 Log.w(TAG, "Failed to configure mParticle with '" + ConfigKeys.ENABLE_COMPRESSION + "' setting");
             }
@@ -110,39 +115,41 @@ import com.mparticle.Constants.MessageType;
         mUploadHandler.sendEmptyMessageDelayed(UploadHandler.CLEANUP, Constants.INITIAL_UPLOAD_DELAY);
     }
 
-    /* package-private */ static JSONObject createMessage(String messageType, String sessionId, long sessionStart, long time, String name, JSONObject attributes, boolean includeConnLoc) throws JSONException {
-            JSONObject message = new JSONObject();
-            message.put(MessageKey.TYPE, messageType);
-            message.put(MessageKey.TIMESTAMP, time);
-            if (MessageType.SESSION_START==messageType) {
-                message.put(MessageKey.ID, sessionId);
-            } else {
-                message.put(MessageKey.SESSION_ID, sessionId);
-                message.put(MessageKey.ID, UUID.randomUUID().toString());
-                if (sessionStart>0) {
-                    message.put(MessageKey.SESSION_START_TIMESTAMP, sessionStart);
-                }
+    /* package-private */static JSONObject createMessage(String messageType, String sessionId, long sessionStart,
+            long time, String name, JSONObject attributes, boolean includeConnLoc) throws JSONException {
+        JSONObject message = new JSONObject();
+        message.put(MessageKey.TYPE, messageType);
+        message.put(MessageKey.TIMESTAMP, time);
+        if (MessageType.SESSION_START == messageType) {
+            message.put(MessageKey.ID, sessionId);
+        } else {
+            message.put(MessageKey.SESSION_ID, sessionId);
+            message.put(MessageKey.ID, UUID.randomUUID().toString());
+            if (sessionStart > 0) {
+                message.put(MessageKey.SESSION_START_TIMESTAMP, sessionStart);
             }
-            if (null != name) {
-                message.put(MessageKey.NAME, name);
+        }
+        if (null != name) {
+            message.put(MessageKey.NAME, name);
+        }
+        if (null != attributes) {
+            message.put(MessageKey.ATTRIBUTES, attributes);
+        }
+        if (includeConnLoc) {
+            message.put(MessageKey.DATA_CONNECTION, sActiveNetworkName);
+            if (null != sLocation) {
+                JSONObject locJSON = new JSONObject();
+                locJSON.put(MessageKey.LATITUDE, sLocation.getLatitude());
+                locJSON.put(MessageKey.LONGITUDE, sLocation.getLongitude());
+                locJSON.put(MessageKey.ACCURACY, sLocation.getAccuracy());
+                message.put(MessageKey.LOCATION, locJSON);
             }
-            if (null != attributes) {
-                message.put(MessageKey.ATTRIBUTES, attributes);
-            }
-            if (includeConnLoc) {
-                message.put(MessageKey.DATA_CONNECTION, sActiveNetworkName);
-                if (null!=sLocation) {
-                    JSONObject locJSON = new JSONObject();
-                    locJSON.put(MessageKey.LATITUDE, sLocation.getLatitude());
-                    locJSON.put(MessageKey.LONGITUDE, sLocation.getLongitude());
-                    locJSON.put(MessageKey.ACCURACY, sLocation.getAccuracy());
-                    message.put(MessageKey.LOCATION, locJSON);
-                }
-            }
-            return message;
+        }
+        return message;
     }
 
-    /* package-private */ static JSONObject createMessageSessionEnd(String sessionId, long start, long end, long length, JSONObject attributes) throws JSONException {
+    /* package-private */static JSONObject createMessageSessionEnd(String sessionId, long start, long end, long length,
+            JSONObject attributes) throws JSONException {
         JSONObject message = createMessage(MessageType.SESSION_END, sessionId, start, end, null, attributes, true);
         message.put(MessageKey.SESSION_LENGTH, length);
         return message;
@@ -160,11 +167,12 @@ import com.mparticle.Constants.MessageType;
 
     public void stopSession(String sessionId, long stopTime, long sessionLength) {
         try {
-            JSONObject sessionTiming=new JSONObject();
+            JSONObject sessionTiming = new JSONObject();
             sessionTiming.put(MessageKey.SESSION_ID, sessionId);
             sessionTiming.put(MessageKey.TIMESTAMP, stopTime);
             sessionTiming.put(MessageKey.SESSION_LENGTH, sessionLength);
-            mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.UPDATE_SESSION_END, sessionTiming));
+            mMessageHandler
+                    .sendMessage(mMessageHandler.obtainMessage(MessageHandler.UPDATE_SESSION_END, sessionTiming));
         } catch (JSONException e) {
             Log.w(TAG, "Failed to send update session end message");
         }
@@ -172,12 +180,14 @@ import com.mparticle.Constants.MessageType;
 
     public void endSession(String sessionId, long stopTime, long sessionLength) {
         stopSession(sessionId, stopTime, sessionLength);
-        mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.CREATE_SESSION_END_MESSAGE, sessionId));
+        mMessageHandler
+                .sendMessage(mMessageHandler.obtainMessage(MessageHandler.CREATE_SESSION_END_MESSAGE, sessionId));
     }
 
     public void logEvent(String sessionId, long sessionStartTime, long time, String eventName, JSONObject attributes) {
         try {
-            JSONObject message = createMessage(MessageType.EVENT, sessionId, sessionStartTime, time, eventName, attributes, true);
+            JSONObject message = createMessage(MessageType.EVENT, sessionId, sessionStartTime, time, eventName,
+                    attributes, true);
             // NOTE: event timing is not supported (yet) but the server expects this data
             message.put(MessageKey.EVENT_START_TIME, time);
             message.put(MessageKey.EVENT_DURATION, 0);
@@ -187,9 +197,11 @@ import com.mparticle.Constants.MessageType;
         }
     }
 
-    public void logScreenView(String sessionId, long sessionStartTime, long time, String screenName, JSONObject attributes) {
+    public void logScreenView(String sessionId, long sessionStartTime, long time, String screenName,
+            JSONObject attributes) {
         try {
-            JSONObject message = createMessage(MessageType.SCREEN_VIEW, sessionId, sessionStartTime, time, screenName, attributes, true);
+            JSONObject message = createMessage(MessageType.SCREEN_VIEW, sessionId, sessionStartTime, time, screenName,
+                    attributes, true);
             mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.STORE_MESSAGE, message));
         } catch (JSONException e) {
             Log.w(TAG, "Failed to create mParticle screen view message");
@@ -198,7 +210,8 @@ import com.mparticle.Constants.MessageType;
 
     public void optOut(String sessionId, long sessionStartTime, long time, boolean optOutStatus) {
         try {
-            JSONObject message = createMessage(MessageType.OPT_OUT, sessionId, sessionStartTime, time, null, null, false);
+            JSONObject message = createMessage(MessageType.OPT_OUT, sessionId, sessionStartTime, time, null, null,
+                    false);
             message.put(MessageKey.OPT_OUT_STATUS, optOutStatus);
             mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.STORE_MESSAGE, message));
         } catch (JSONException e) {
@@ -209,7 +222,7 @@ import com.mparticle.Constants.MessageType;
     public void logErrorEvent(String sessionId, long sessionStartTime, long time, String errorMessage, Throwable t) {
         try {
             JSONObject message = createMessage(MessageType.ERROR, sessionId, sessionStartTime, time, null, null, false);
-            if (null!=t) {
+            if (null != t) {
                 message.put(MessageKey.ERROR_SEVERITY, "fatal");
                 message.put(MessageKey.ERROR_CLASS, t.getClass().getCanonicalName());
                 message.put(MessageKey.ERROR_MESSAGE, t.getMessage());
@@ -228,7 +241,8 @@ import com.mparticle.Constants.MessageType;
 
     public void setPushRegistrationId(String token, boolean registeringFlag) {
         try {
-            JSONObject message = createMessage(MessageType.PUSH_REGISTRATION, null, 0, System.currentTimeMillis(), null, null, false);
+            JSONObject message = createMessage(MessageType.PUSH_REGISTRATION, null, 0, System.currentTimeMillis(),
+                    null, null, false);
             message.put(MessageKey.PUSH_TOKEN, token);
             message.put(MessageKey.PUSH_REGISTER_FLAG, registeringFlag);
             mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.STORE_MESSAGE, message));
@@ -239,10 +253,11 @@ import com.mparticle.Constants.MessageType;
 
     public void setSessionAttributes(String sessionId, JSONObject mSessionAttributes) {
         try {
-            JSONObject sessionAttributes=new JSONObject();
+            JSONObject sessionAttributes = new JSONObject();
             sessionAttributes.put(MessageKey.SESSION_ID, sessionId);
             sessionAttributes.put(MessageKey.ATTRIBUTES, mSessionAttributes);
-            mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.UPDATE_SESSION_ATTRIBUTES, sessionAttributes));
+            mMessageHandler.sendMessage(mMessageHandler.obtainMessage(MessageHandler.UPDATE_SESSION_ATTRIBUTES,
+                    sessionAttributes));
         } catch (JSONException e) {
             Log.w(TAG, "Failed to send update session attributes message");
         }
@@ -253,7 +268,7 @@ import com.mparticle.Constants.MessageType;
     }
 
     public static void setLocation(Location location) {
-        sLocation=location;
+        sLocation = location;
         if (sDebugMode) {
             Log.d(TAG, "Received location update: " + location);
         }
@@ -264,7 +279,7 @@ import com.mparticle.Constants.MessageType;
     }
 
     public void setSecureTransport(boolean sslEnabled) {
-        mUploadHandler.setConnectionScheme( sslEnabled ? "https" : "http" );
+        mUploadHandler.setConnectionScheme(sslEnabled ? "https" : "http");
     }
 
     public void setUploadInterval(int uploadInterval) {
@@ -275,7 +290,8 @@ import com.mparticle.Constants.MessageType;
         @Override
         public void onReceive(Context appContext, Intent intent) {
             if (ConnectivityManager.CONNECTIVITY_ACTION.equals(intent.getAction())) {
-                ConnectivityManager connectivyManager = (ConnectivityManager)appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+                ConnectivityManager connectivyManager = (ConnectivityManager) appContext
+                        .getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo activeNetwork = connectivyManager.getActiveNetworkInfo();
                 MessageManager.setDataConnection(activeNetwork);
             }
@@ -283,9 +299,9 @@ import com.mparticle.Constants.MessageType;
     }
 
     public static void setDataConnection(NetworkInfo activeNetwork) {
-        if (null!=activeNetwork) {
+        if (null != activeNetwork) {
             String activeNetworkName = activeNetwork.getTypeName();
-            if (0!=activeNetwork.getSubtype()) {
+            if (0 != activeNetwork.getSubtype()) {
                 activeNetworkName += "/" + activeNetwork.getSubtypeName();
             }
             sActiveNetworkName = activeNetworkName.toLowerCase(Locale.US);
