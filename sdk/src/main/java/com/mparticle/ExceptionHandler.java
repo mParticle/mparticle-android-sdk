084020587b2f300c37ac8b8ac8dcb43626bae9eb
Samuel Dozor <sdozor@mparticle.com>
2014-01-15 13:27:18 -0500

    Make exception handler is the MParticleAPI

--- sdk/src/main/java/com/mparticle/ExceptionHandler.java
+++ sdk/src/main/java/com/mparticle/ExceptionHandler.java
@@ -18,7 +18,7 @@ import android.util.Log;
     @Override
     public void uncaughtException(Thread thread, Throwable ex) {
         try {
-            mMessageManager.logErrorEvent(null, 0, System.currentTimeMillis(), ex != null ? ex.getMessage() : null, ex, null, false);
+            MParticleAPI.getInstance(null).logUnhandledError(ex);
             if (null != mOriginalUncaughtExceptionHandler) {
                 mOriginalUncaughtExceptionHandler.uncaughtException(thread, ex);
             } else {
