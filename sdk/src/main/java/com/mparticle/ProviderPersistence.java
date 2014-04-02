aa56106dd0f7166e004b7b4999f666a91e013ef3
Samuel Dozor <sdozor@mparticle.com>
2014-04-02 17:00:35 -0400

    Writing cms preferences to storage so that generated IDs can be re-used across requests.

--- sdk/src/main/java/com/mparticle/ProviderPersistence.java
+++ sdk/src/main/java/com/mparticle/ProviderPersistence.java
@@ -22,6 +22,7 @@ public class ProviderPersistence extends JSONObject{
     private static final String KEY_PERSISTENCE_TYPE = "t";
     private static final String KEY_PERSISTENCE_MPVAR = "n";
     private static final String KEY_PERSISTENCE_DEFAULT = "d";
+    private static final String MPPREFIX = "mp::";
 
     private static final int PERSISTENCE_TYPE_STRING = 1;
     private static final int PERSISTENCE_TYPE_INT = 2;
@@ -43,36 +44,75 @@ public class ProviderPersistence extends JSONObject{
                     JSONObject fileObject = files.getJSONObject(fileIndex);
                     SharedPreferences preferences = context.getSharedPreferences(fileObject.getString(KEY_PERSISTENCE_FILE), fileObject.getInt(KEY_PERSISTENCE_MODE));
                     JSONArray fileObjects = fileObject.getJSONArray(KEY_PERSISTENCE_KEY_LIST);
-
+                    SharedPreferences.Editor editor = preferences.edit();
                     for (int keyIndex = 0; keyIndex < fileObjects.length(); keyIndex++){
                         final int type = fileObjects.getJSONObject(keyIndex).getInt(KEY_PERSISTENCE_TYPE);
                         final String key = fileObjects.getJSONObject(keyIndex).getString(KEY_PERSISTENCE_KEY);
                         final String mpKey = fileObjects.getJSONObject(keyIndex).getString(KEY_PERSISTENCE_MPVAR);
+                        final String mpPersistenceKey = MPPREFIX + mpKey;
+                        boolean contains = preferences.contains(key);
+                        boolean containsMpKey = preferences.contains(mpPersistenceKey);
                         Object defaultValue;
-                        switch (type){
+                        switch (type) {
                             case PERSISTENCE_TYPE_STRING:
                                 defaultValue = fileObjects.getJSONObject(keyIndex).getString(KEY_PERSISTENCE_DEFAULT);
-                                values.put(mpKey, preferences.getString(key, (String)defaultValue));
+                                if (contains) {
+                                    values.put(mpKey, preferences.getString(key, (String) defaultValue));
+                                } else {
+                                    if (!containsMpKey){
+                                        editor.putString(mpPersistenceKey , (String) defaultValue);
+                                    }
+                                    values.put(mpKey, preferences.getString(mpPersistenceKey, (String) defaultValue));
+                                }
                                 break;
                             case PERSISTENCE_TYPE_INT:
                                 defaultValue = fileObjects.getJSONObject(keyIndex).getInt(KEY_PERSISTENCE_DEFAULT);
-                                values.put(mpKey, preferences.getInt(key, (Integer) defaultValue));
+                                if (contains) {
+                                    values.put(mpKey, preferences.getInt(key, (Integer) defaultValue));
+                                } else {
+                                    if (!containsMpKey){
+                                        editor.putInt(mpPersistenceKey, (Integer) defaultValue);
+                                    }
+                                    values.put(mpKey, preferences.getInt(mpPersistenceKey, (Integer) defaultValue));
+                                }
                                 break;
                             case PERSISTENCE_TYPE_BOOLEAN:
                                 defaultValue = fileObjects.getJSONObject(keyIndex).getBoolean(KEY_PERSISTENCE_DEFAULT);
-                                values.put(mpKey, preferences.getBoolean(key, (Boolean) defaultValue));
+                                if (contains) {
+                                    values.put(mpKey, preferences.getBoolean(key, (Boolean) defaultValue));
+                                } else {
+                                    if (!containsMpKey){
+                                        editor.putBoolean(mpPersistenceKey, (Boolean) defaultValue);
+                                    }
+                                    values.put(mpKey, preferences.getBoolean(mpPersistenceKey, (Boolean) defaultValue));
+                                }
                                 break;
                             case PERSISTENCE_TYPE_FLOAT:
                                 defaultValue = fileObjects.getJSONObject(keyIndex).getDouble(KEY_PERSISTENCE_DEFAULT);
-                                values.put(mpKey, preferences.getFloat(key, (Float) defaultValue));
+                                if (contains) {
+                                    values.put(mpKey, preferences.getFloat(key, (Float) defaultValue));
+                                } else {
+                                    if (!containsMpKey){
+                                        editor.putFloat(mpPersistenceKey, (Float) defaultValue);
+                                    }
+                                    values.put(mpKey, preferences.getFloat(mpPersistenceKey, (Float) defaultValue));
+                                }
                                 break;
                             case PERSISTENCE_TYPE_LONG:
                                 defaultValue = fileObjects.getJSONObject(keyIndex).getLong(KEY_PERSISTENCE_DEFAULT);
-                                values.put(mpKey, preferences.getLong(key, (Long) defaultValue));
+                                if (contains) {
+                                    values.put(mpKey, preferences.getLong(key, (Long) defaultValue));
+                                } else {
+                                    if (!containsMpKey){
+                                        editor.putLong(mpPersistenceKey, (Long) defaultValue);
+                                    }
+                                    values.put(mpKey, preferences.getLong(mpPersistenceKey, (Long) defaultValue));
+                                }
                                 break;
                         }
-
+                        editor.commit();
                     }
+
                 }
             }
             put(Integer.toString(configPersistence.getJSONObject(i).getInt(KEY_PERSISTENCE_ID)), values);
