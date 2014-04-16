a7df69d54b4669061e6f13dbb5fde2da5d311acf
Samuel Dozor <sdozor@mparticle.com>
2014-04-16 17:48:53 -0400

    Reducing the visibility of eks interface

--- sdk/src/main/java/com/mparticle/IEmbeddedKit.java
+++ sdk/src/main/java/com/mparticle/IEmbeddedKit.java
@@ -7,7 +7,7 @@ import org.json.JSONObject;
 /**
  * Created by sdozor on 3/14/14.
  */
-public interface IEmbeddedKit {
+interface IEmbeddedKit {
     void logEvent(MParticle.EventType type, String name, JSONObject eventAttributes) throws Exception;
     void logTransaction(MPProduct transaction) throws Exception;
     void logScreen(String screenName, JSONObject eventAttributes) throws Exception;
