7dea6271c06496cd8007a081a6d7aa7ba7fb5b3c
Samuel Dozor <sdozor@mparticle.com>
2014-06-12 15:05:37 -0400

    Making the server url configurable by a build file.

--- sdk/src/main/java/com/mparticle/MParticleApiClient.java
+++ sdk/src/main/java/com/mparticle/MParticleApiClient.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.util.Base64;
 import android.util.Log;
 
+
 import org.apache.http.HttpStatus;
 import org.apache.http.impl.cookie.DateUtils;
 import org.apache.http.protocol.HTTP;
@@ -45,10 +46,8 @@ class MParticleApiClient {
 
     public static final String HEADER_SIGNATURE = "x-mp-signature";
     public static final String SECURE_SERVICE_SCHEME = "https";
-    public static final String SECURE_SERVICE_HOST = "nativesdks.mparticle.com";
-    //public static final String SECURE_SERVICE_HOST = "54.236.165.123";
-    //public static final String SECURE_SERVICE_HOST = "api-qa.mparticle.com";
-    //public static final String SECURE_SERVICE_HOST = "10.0.16.21";
+    public static final String SECURE_SERVICE_HOST = BuildConfig.MP_URL;
+
     public static final String SERVICE_VERSION_1 = "/v1";
     public static final String SERVICE_VERSION_2 = "/v2";
     public static final String COOKIES = "ck";
