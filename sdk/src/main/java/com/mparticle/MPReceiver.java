16e7517c58289352a45dda3ced07c72c81ceee3e
Sam Dozor <sdozor@mparticle.com>
2014-10-15 18:01:16 -0400

    Adding protected methods, allowing customers to listen for push hooks.

--- sdk/src/main/java/com/mparticle/MPReceiver.java
+++ sdk/src/main/java/com/mparticle/MPReceiver.java
@@ -1,6 +1,7 @@
 package com.mparticle;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -56,17 +57,51 @@ public class MPReceiver extends BroadcastReceiver {
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public final void onReceive(Context context, Intent intent) {
         if (!MPARTICLE_IGNORE.equals(intent.getAction())) {
             if ("com.android.vending.INSTALL_REFERRER".equals(intent.getAction())) {
                 String referrer = intent.getStringExtra("referrer");
                 SharedPreferences preferences = context.getSharedPreferences(Constants.PREFS_FILE, Context.MODE_PRIVATE);
                 preferences.edit().putString(Constants.PrefKeys.INSTALL_REFERRER, referrer).commit();
+            } else if (MParticlePushUtility.BROADCAST_NOTIFICATION_TAPPED.equalsIgnoreCase(intent.getAction())){
+                AbstractCloudMessage message = intent.getParcelableExtra(MParticlePushUtility.CLOUD_MESSAGE_EXTRA);
+                AbstractCloudMessage.CloudAction action = intent.getParcelableExtra(MParticlePushUtility.CLOUD_ACTION_EXTRA);
+                if (!onNotificationTapped(message, action)){
+                    MPService.runIntentInService(context, intent);
+                }
+            } else if (MParticlePushUtility.BROADCAST_NOTIFICATION_RECEIVED.equalsIgnoreCase(intent.getAction())){
+                AbstractCloudMessage message = intent.getParcelableExtra(MParticlePushUtility.CLOUD_MESSAGE_EXTRA);
+                if (!onNotificationReceived(message)){
+                    MPService.runIntentInService(context, intent);
+                }
             } else {
-                MParticle.start(context);
                 MPService.runIntentInService(context, intent);
             }
         }
         setResult(Activity.RESULT_OK, null, null);
     }
+
+    /**
+     * Override this method to listen for when a notification has been received.
+     *
+     *
+     * @param message The message that was received. Depending on the push provider, could be either a {@link com.mparticle.MPCloudMessage} or a {@link com.mparticle.ProviderCloudMessage}
+     * @return True if you would like to handle this notification, False if you would like the mParticle to generate and show a {@link android.app.Notification}.
+     */
+    protected boolean onNotificationReceived(AbstractCloudMessage message){
+        return false;
+    }
+
+    /**
+     * Override this method to listen for when a notification has been tapped or acted on.
+     *
+     *
+     * @param message The message that was tapped. Depending on the push provider, could be either a {@link com.mparticle.MPCloudMessage} or a {@link com.mparticle.ProviderCloudMessage}
+     * @param action The action that the user acted on.
+     * @return True if you would like to consume this tap/action, False if the mParticle SDK should attempt to handle it.
+     */
+    protected boolean onNotificationTapped(AbstractCloudMessage message, AbstractCloudMessage.CloudAction action){
+        return false;
+    }
+
 }
