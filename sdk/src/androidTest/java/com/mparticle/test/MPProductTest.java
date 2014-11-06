d6eb7cd34fa6ed8b92920cbc13dd1a78575ca896
Sam Dozor <sdozor@mparticle.com>
2014-11-06 13:52:33 -0500

    Adding some more sanity and mpproduct tests

---
+++ sdk/src/androidTest/java/com/mparticle/test/MPProductTest.java
@@ -0,0 +1,18 @@
+package com.mparticle.test;
+
+import android.test.AndroidTestCase;
+
+import com.mparticle.MPProduct;
+
+/**
+ * Created by sdozor on 11/6/14.
+ */
+public class MPProductTest extends AndroidTestCase {
+
+    public void testBuilder(){
+        MPProduct product = new MPProduct.Builder("test product name", "test-product-sku-0").build();
+        assertNotNull(product);
+        assertEquals(product.getProductName(), "test product name");
+        assertEquals(product.get("ProductSKU"), "test-product-sku-0");
+    }
+}
