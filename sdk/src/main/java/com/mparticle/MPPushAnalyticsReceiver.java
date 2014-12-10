0be85ff4c0c3725ab42ce4cb0ba8717083142949
Sam Dozor <sdozor@mparticle.com>
2014-12-10 11:22:02 -0500

    Adding a 2nd receiver for clients to inherit from.

---
+++ sdk/src/main/java/com/mparticle/MPPushAnalyticsReceiver.java
@@ -0,0 +1,55 @@
+package com.mparticle;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.mparticle.messaging.AbstractCloudMessage;
+import com.mparticle.messaging.CloudAction;
+
+public class MPPushAnalyticsReceiver extends BroadcastReceiver {
+    @Override
+    public final void onReceive(Context context, Intent intent) {
+        if (MParticlePushUtility.BROADCAST_NOTIFICATION_TAPPED.equalsIgnoreCase(intent.getAction())){
+            AbstractCloudMessage message = intent.getParcelableExtra(MParticlePushUtility.CLOUD_MESSAGE_EXTRA);
+            CloudAction action = intent.getParcelableExtra(MParticlePushUtility.CLOUD_ACTION_EXTRA);
+            if (!onNotificationTapped(message, action)){
+                MPService.runIntentInService(context, intent);
+            }
+            return;
+        } else if (MParticlePushUtility.BROADCAST_NOTIFICATION_RECEIVED.equalsIgnoreCase(intent.getAction())){
+            AbstractCloudMessage message = intent.getParcelableExtra(MParticlePushUtility.CLOUD_MESSAGE_EXTRA);
+            if (!onNotificationReceived(message)){
+                MPService.runIntentInService(context, intent);
+            }
+            return;
+        }
+
+    }
+
+
+
+    /**
+     * Override this method to listen for when a notification has been received.
+     *
+     *
+     * @param message The message that was received. Depending on the push provider, could be either a {@link com.mparticle.messaging.MPCloudNotificationMessage} or a {@link com.mparticle.messaging.ProviderCloudMessage}
+     * @return True if you would like to handle this notification, False if you would like the mParticle to generate and show a {@link android.app.Notification}.
+     */
+    protected boolean onNotificationReceived(AbstractCloudMessage message){
+        return false;
+    }
+
+    /**
+     * Override this method to listen for when a notification has been tapped or acted on.
+     *
+     *
+     * @param message The message that was tapped. Depending on the push provider, could be either a {@link com.mparticle.messaging.MPCloudNotificationMessage} or a {@link com.mparticle.messaging.ProviderCloudMessage}
+     * @param action The action that the user acted on.
+     * @return True if you would like to consume this tap/action, False if the mParticle SDK should attempt to handle it.
+     */
+    protected boolean onNotificationTapped(AbstractCloudMessage message, CloudAction action){
+        return false;
+    }
+
+}
