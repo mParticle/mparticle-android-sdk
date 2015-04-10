64c314b703f014f0388e20a7205416892d1ea5a8
Sam Dozor <sdozor@mparticle.com>
2015-04-10 09:37:37 -0400

    Mocking package manager/info/etc

--- sdk/src/test/java/com/mparticle/MockContext.java
+++ sdk/src/test/java/com/mparticle/MockContext.java
@@ -3,7 +3,14 @@ package com.mparticle;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static junit.framework.Assert.fail;
 
 public class MockContext extends android.test.mock.MockContext {
 
@@ -21,6 +28,25 @@ public class MockContext extends android.test.mock.MockContext {
     @Override
     public SharedPreferences getSharedPreferences(String name, int mode) {
         return sharedPreferences;
+    }
+
+    @Override
+    public PackageManager getPackageManager() {
+        PackageManager manager = Mockito.mock(PackageManager.class);
+        PackageInfo info = Mockito.mock(PackageInfo.class);
+        info.versionName = "42";
+        info.versionCode = 42;
+        ApplicationInfo appInfo = Mockito.mock(ApplicationInfo.class);
+        try {
+            Mockito.when(manager.getPackageInfo(Mockito.anyString(), Mockito.anyInt())).thenReturn(info);
+            Mockito.when(manager.getInstallerPackageName(Mockito.anyString())).thenReturn("com.mparticle.test.installer");
+
+            Mockito.when(manager.getApplicationInfo(Mockito.anyString(), Mockito.anyInt())).thenReturn(appInfo);
+            Mockito.when(manager.getApplicationLabel(appInfo)).thenReturn("test label");
+        }catch (Exception e){
+            fail(e.toString());
+        }
+        return manager;
     }
 
     @Override
