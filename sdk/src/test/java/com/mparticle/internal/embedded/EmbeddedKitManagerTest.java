1d8b253e36c1586e576a0fe9a6b243a943d9e038
Samuel Dozor <samuel.dozor@gmail.com>
2015-04-11 09:05:57 -0400

    Stubbing EK manager test cases.

---
+++ sdk/src/test/java/com/mparticle/internal/embedded/EmbeddedKitManagerTest.java
@@ -0,0 +1,78 @@
+package com.mparticle.internal.embedded;
+
+import junit.framework.TestCase;
+
+public class EmbeddedKitManagerTest extends TestCase {
+
+    public void testUpdateKits() throws Exception {
+
+    }
+
+    public void testLogEvent() throws Exception {
+
+    }
+
+    public void testLogTransaction() throws Exception {
+
+    }
+
+    public void testLogScreen() throws Exception {
+
+    }
+
+    public void testSetLocation() throws Exception {
+
+    }
+
+    public void testSetUserAttributes() throws Exception {
+
+    }
+
+    public void testRemoveUserAttribute() throws Exception {
+
+    }
+
+    public void testSetUserIdentity() throws Exception {
+
+    }
+
+    public void testLogout() throws Exception {
+
+    }
+
+    public void testOnActivityStarted() throws Exception {
+
+    }
+
+    public void testIsEmbeddedKitUri() throws Exception {
+
+    }
+
+    public void testGetActiveModuleIds() throws Exception {
+
+    }
+
+    public void testGetSurveyUrl() throws Exception {
+
+    }
+
+    public void testGetContext() throws Exception {
+
+    }
+
+    public void testGetConfigurationManager() throws Exception {
+
+    }
+
+    public void testGetAppStateManager() throws Exception {
+
+    }
+
+    public void testSetConfigManager() throws Exception {
+
+    }
+
+    public void testSetAppStateManager() throws Exception {
+
+    }
+}
