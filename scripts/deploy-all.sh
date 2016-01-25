718c0639432e2776ba00da1a16fcb043e83494d8
Sam Dozor <sdozor@mparticle.com>
2016-01-25 17:21:12 -0500

    Use project dependencies, not maven dependencies
    Prior to this commit it was tricky to get the gradle maven plugin to correctly
    name each dependency in the various generated POMs. Because of this, each dependency
    was specified as a maven dependency, rather than a project dependency, to tell the maven
    plugin what everything should be called. This update changes a few things:
    
    - Import and name projects in settings.gradle
    - In addition to name, set moduleId and version at the project level
    - Dynamically add kit project dependencies to the all-kits artifact
    - Remove the need for gradle.properties in every kit project
    
    Given the above, the project can always be built and uploaded with a single command,
    even as the version number changes.

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-../gradlew -c settings-core.gradle clean test uploadArchives -Ptarget_maven_repo=$1
-../gradlew -c settings-kits.gradle clean test uploadArchives -Ptarget_maven_repo=$1 
-../gradlew -c settings-allkits.gradle clean test uploadArchives -Ptarget_maven_repo=$1
+../gradlew clean test uploadArchives -Ptarget_maven_repo=$1
