cf5087987ee04d3055219c4df5a88b7b6e294e83
Sam Dozor <sdozor@mparticle.com>
2018-11-09 12:38:10 -0500

    Add release script

---
+++ scripts/release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+: ${1?"Version string missing - usage: $0 x.y.z"}
+
+#update build.gradle
+sed -i '.bak' "s/version = '.*-SNAPSHOT/version = '$1-SNAPSHOT/g" build.gradle
+
+#update README.md
+sed -i '.bak' "s/'com.mparticle:android-core:.*'/'com.mparticle:android-core:$1'/g" README.md
+sed -i '.bak' "s/'com.mparticle:android-example-kit:.*'/'com.mparticle:android-example-kit:$1'/g" README.md
+sed -i '.bak' "s/'com.mparticle:android-another-kit:.*'/'com.mparticle:android-another-kit:$1'/g" README.md
+
+#commit the version bump, tag, and push to private and public
+git add build.gradle
+git add README.md
+git commit -m "Update version to $1"
+git tag "v$1"
+git push origin "v$1"
+git push public "v$1"
+git push origin development
+git push origin HEAD:master
+git push public HEAD:master
