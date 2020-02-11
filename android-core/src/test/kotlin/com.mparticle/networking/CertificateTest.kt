040c611f974276d0a89e792bbffba14b1a52baf2
Will Passidomo <wpassidomo@mparticle.com>
2020-02-11 16:16:17 -0500

    Configure Kotlin for tests

---
+++ android-core/src/test/kotlin/com.mparticle/networking/CertificateTest.kt
@@ -0,0 +1,30 @@
+package com.mparticle.networking;
+
+import com.mparticle.testutils.RandomUtils
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNull
+import org.junit.Test
+
+
+class CertificateTest {
+    val randomUtils = RandomUtils()
+    val alias = randomUtils.getAlphaNumericString(10);
+    val certificateString = randomUtils.getAlphaNumericString(124);
+
+    @Test
+    fun buildCertificateTest() {
+        val certificate = Certificate.with(alias, certificateString);
+        assertEquals(alias, certificate?.getAlias());
+        assertEquals(certificateString, certificate?.getCertificate());
+    }
+
+    @Test
+    fun rejectMalformedCertificate() {
+        var certificate = Certificate.with("", certificateString);
+        assertNull(certificate);
+
+        certificate = Certificate.with(alias, "");
+        assertNull(certificate);
+
+    }
+}
