a18608954cf5243d0755a870fa6282dc6839aa3d
Will Passidomo <wpassidomo@mparticle.com>
2020-09-25 17:24:37 -0400

    Fix release scripts

--- scripts/kits/merge-updates.sh
+++ scripts/kits/merge-updates.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 : "${2?"Kit Test Branch missing - usage: $0 \{branch_name\} \{target_branch\}"}"
 
-CURRENT_BRANCE=`git rev-parse --abbrev-ref HEAD`
+CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo "Fetch"
 git fetch origin
 echo "Rebase"
 git rebase origin/$1
 echo "Push Updated Branch"
-git push origin
+git push origin $2
 echo "Delete Kit Feature branch Locally"
 git branch -D $1
 echo "Push Delete remote"
 git push origin --delete $1
-git checkout $CURRENT_BRANCE
+git checkout $CURRENT_BRANCH
--- scripts/kits/update.sh
+++ scripts/kits/update.sh
@@ -3,14 +3,15 @@
 
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 # checkout a kit branch for testing
+git branch -D $1
 git checkout -b $1
 
 # update kit references
-git submodule foreach "git checkout master; git fetch; git pull origin master";
+git submodule foreach "git fetch; git reset --hard origin/master";
 
 # commit kit reference deltas and push to private
 git add kits/*;
 git commit -m "Update submodules"
-git push origin $1
+git push -f origin $1
 
 git checkout $CURRENT_BRANCH
