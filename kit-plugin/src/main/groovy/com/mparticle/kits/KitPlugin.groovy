c5335554541ddffcdce0d991b897b5e0f112310c
Michael Chuang <michael.t.chuang@gmail.com>
2021-09-13 11:04:09 -0700

    fix: upgrade build tool version

--- kit-plugin/src/main/groovy/com/mparticle/kits/KitPlugin.groovy
+++ kit-plugin/src/main/groovy/com/mparticle/kits/KitPlugin.groovy
@@ -27,7 +27,7 @@ class KitPlugin implements Plugin<Project> {
         target.extensions.create("mparticle", MParticlePluginExtension)
         LibraryExtension androidLib = target.android
         androidLib.compileSdkVersion(30)
-        androidLib.buildToolsVersion('30.0.2')
+        androidLib.buildToolsVersion('30.0.3')
         androidLib.defaultConfig.versionCode = Integer.parseInt(new Date().format('yyyyMMdd'))
         androidLib.defaultConfig.minSdkVersion 14
         androidLib.defaultConfig.targetSdkVersion 30
