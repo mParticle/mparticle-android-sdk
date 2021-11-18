253c9a32aea894bfa1c975c546965a3c75aa900a
Will Passidomo <willpassidomo@users.noreply.github.com>
2021-11-18 13:48:23 -0500

    fix: bad formatting in release.config.js (#75)

--- release.config.js
+++ release.config.js
@@ -6,19 +6,19 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         preset: "angular",
-      },
-      "releaseRules": [
-        {type: 'feat', release: 'minor'},
-        {type: 'ci', release: 'patch'},
-        {type: 'fix', scope: 'patch'},
-        {type: 'docs', release: 'patch'},
-        {type: 'test', release: 'patch'},
-        {type: 'refactor', release: 'patch'},
-        {type: 'style', release: 'patch'},
-        {type: 'build', release: 'patch'},
-        {type: 'chore', release: 'patch'},
-        {type: 'revert', release: 'patch'}
+        releaseRules: [
+            {type: 'feat', release: 'minor'},
+            {type: 'ci', release: 'patch'},
+            {type: 'fix', scope: 'patch'},
+            {type: 'docs', release: 'patch'},
+            {type: 'test', release: 'patch'},
+            {type: 'refactor', release: 'patch'},
+            {type: 'style', release: 'patch'},
+            {type: 'build', release: 'patch'},
+            {type: 'chore', release: 'patch'},
+            {type: 'revert', release: 'patch'}
        ]
+      }
     ],
     [
       "@semantic-release/release-notes-generator",
