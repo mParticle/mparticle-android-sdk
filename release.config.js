236765388138a891cdb60ff6fbb50112edad42a3
Michael Chuang <mchuang@mparticle.com>
2022-03-08 10:51:49 -0800

    fix: add etag to batch upload payload

--- release.config.js
+++ release.config.js
@@ -44,7 +44,7 @@ module.exports = {
       {
         assets: ["CHANGELOG.md", "build.gradle", "README.md"],
         message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}",
       },
     ],
   ],
