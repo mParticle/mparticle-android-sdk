60ee0428debd5d72d5c0a039d425a66ef32fe165
mchuangatmp <mchuang@mparticle.com>
2022-05-13 09:26:47 -0700

    ci: add in release notes generator updates

--- release.config.js
+++ release.config.js
@@ -23,8 +23,57 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
-        preset: "angular",
-      },
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              "type": "feat",
+              "section": "Features",
+              "hidden": false
+            },
+            {
+              "type": "fix",
+              "section": "Bug Fixes",
+              "hidden": false
+            },
+            {
+              "type": "docs",
+              "section": "Documentation",
+              "hidden": false
+            },
+            {
+              "type": "style",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            },
+            {
+              "type": "refactor",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            },
+            {
+              "type": "perf",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            },
+            {
+              "type": "test",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            },
+            {
+              "type": "ci",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            },
+            {
+              "type": "chore",
+              "section": "Updates & Maintenance",
+              "hidden": false
+            }
+          ]
+        }
+      }
     ],
     [
       "@semantic-release/changelog",
