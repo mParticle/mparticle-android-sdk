271bdfc6322f1663ad43843d53ea61c5af04c473
Will Passidomo <wpassidomo@mparticle.com>
2022-05-19 13:30:02 -0400

    test: add BatchCreationListener integration tests

---
+++ android-core/src/androidTest/kotlin/com.mparticle/BatchCreationCallbackTests.kt
@@ -0,0 +1,189 @@
+package com.mparticle
+
+import com.mparticle.internal.Constants
+import com.mparticle.networking.Matcher
+import com.mparticle.testutils.BaseCleanInstallEachTest
+import com.mparticle.testutils.TestingUtils.assertJsonEqual
+import org.json.JSONArray
+import org.json.JSONObject
+import org.junit.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class BatchCreationCallbackTests : BaseCleanInstallEachTest() {
+
+    @Test
+    fun testListenerNoChange() {
+        var receivedBatch: JSONObject? = null
+        val options = MParticleOptions.builder(mContext)
+            .batchCreationListener {
+                receivedBatch = JSONObject(it.toString())
+                it
+            }
+        startMParticle(options)
+
+        MParticle.getInstance()?.apply {
+            logEvent(
+                MPEvent.Builder("test event")
+                    .build()
+            )
+            upload()
+        }
+
+        mServer.waitForVerify(
+            Matcher(mServer.Endpoints().eventsUrl).bodyMatch {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == "test event" }
+                    ?.also { isMatch ->
+                        val modified = it.remove(Constants.MessageKey.MODIFIED_BATCH)
+                        assertTrue(modified.toString().toBooleanStrict())
+                        if (isMatch) {
+                            // check new key
+                            assertJsonEqual(it, receivedBatch)
+                        }
+                    } ?: false
+            }
+        )
+    }
+
+    @Test
+    fun testListenerModified() {
+        var newBatch = JSONObject().put("the whole", "batch")
+        val targetEventName = "should not send"
+
+        val options = MParticleOptions.builder(mContext)
+            .batchCreationListener {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == targetEventName }
+                    ?.let { result ->
+                        if (result) {
+                            JSONObject(newBatch.toString())
+                        } else {
+                            it
+                        }
+                    } ?: it
+            }
+        startMParticle(options)
+
+        MParticle.getInstance()?.apply {
+            logEvent(
+                MPEvent.Builder(targetEventName)
+                    .build()
+            )
+            upload()
+        }
+
+        mServer.waitForVerify(
+            Matcher(mServer.Endpoints().eventsUrl).bodyMatch {
+                val modified = it.remove(Constants.MessageKey.MODIFIED_BATCH)
+                assertTrue(modified.toString().toBooleanStrict())
+                it.toString() == newBatch.toString()
+            }
+        )
+
+        // make sure the upload queue is cleared
+        MParticle.getInstance()?.apply {
+            logEvent(MPEvent.Builder("test").build())
+            upload()
+        }
+
+        mServer.waitForVerify(
+            Matcher(mServer.Endpoints().eventsUrl).bodyMatch {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == "test" } ?: false
+            }
+        )
+
+        mServer.Requests().events.any {
+            it.bodyJson.optJSONArray("msgs")
+                ?.toList()
+                ?.filterIsInstance<JSONObject>()
+                ?.any { it.optString("n") == targetEventName } ?: false
+        }.let {
+            assertFalse { it }
+        }
+    }
+
+    @Test
+    fun testListenerCrashes() {
+        var newBatch = JSONObject().put("the whole", "batch")
+        val targetEventName = "should not send"
+
+        val options = MParticleOptions.builder(mContext)
+            .batchCreationListener {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == targetEventName }
+                    ?.let { result ->
+                        if (result) {
+                            throw RuntimeException()
+                        } else {
+                            it
+                        }
+                    } ?: it
+            }
+        startMParticle(options)
+
+        MParticle.getInstance()?.apply {
+            logEvent(MPEvent.Builder(targetEventName).build())
+            upload()
+        }
+
+        // make sure the upload queue is cleared
+        MParticle.getInstance()?.apply {
+            logEvent(MPEvent.Builder("test").build())
+            upload()
+        }
+
+        mServer.waitForVerify(
+            Matcher(mServer.Endpoints().eventsUrl).bodyMatch {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == "test" } ?: false
+            }
+        )
+
+        mServer.Requests().events.any {
+            it.bodyJson.optJSONArray("msgs")
+                ?.toList()
+                ?.filterIsInstance<JSONObject>()
+                ?.any { it.optString("n") == targetEventName } ?: false
+        }.let {
+            assertFalse { it }
+        }
+    }
+
+    @Test
+    fun testNoMPWhenNoListener() {
+        startMParticle()
+        MParticle.getInstance()?.apply {
+            logEvent(MPEvent.Builder("test").build())
+            upload()
+        }
+
+        mServer.waitForVerify(
+            Matcher(mServer.Endpoints().eventsUrl).bodyMatch {
+                it.optJSONArray("msgs")
+                    ?.toList()
+                    ?.filterIsInstance<JSONObject>()
+                    ?.any { it.optString("n") == "test" }
+                    ?.also { result ->
+                        if (result) {
+                            assertNull(it.opt(Constants.MessageKey.MODIFIED_BATCH))
+                        }
+                    } ?: false
+            }
+        )
+    }
+}
+
+fun JSONArray.toList(): List<Any> = (0 until length()).map { this.get(it) }
