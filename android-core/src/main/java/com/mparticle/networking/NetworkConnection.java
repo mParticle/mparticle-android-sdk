66f5352252c15800b0d3c43bc0600284f4dc295b
Adrian Kim <akim@mparticle.com>
2022-10-26 11:36:28 -0400

    Forgot an import

--- android-core/src/main/java/com/mparticle/networking/NetworkConnection.java
+++ android-core/src/main/java/com/mparticle/networking/NetworkConnection.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
