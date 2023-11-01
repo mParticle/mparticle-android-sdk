fc63aa11683bd8b95fb8300fadcb9a9753fe0eec
markvdouw <v-mvanderouw@mparticle.com>
2023-11-01 11:19:30 -0300

    fix: kit syntax for lint

--- android-core/src/main/java/com/mparticle/internal/SideloadedKitsUtils.kt
+++ android-core/src/main/java/com/mparticle/internal/SideloadedKitsUtils.kt
@@ -17,7 +17,8 @@ object SideloadedKitsUtils {
                 }
             }
         }
-        kits.forEach { kit ->
+        for (i in kits.indices) {
+            val kit = kits.get(i)
             if (!addedIds.contains(kit.kitId())) {
                 kit.getJsonConfig()?.let {
                     results.put(it)
