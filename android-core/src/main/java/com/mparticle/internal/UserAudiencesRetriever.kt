0fb9f5d06cd90747b9d0f0f3fede84773135cb88
Mansi Pandya <mpandya@mparticle.com>
2024-04-18 11:38:13 -0400

    fix kotlin lint error

--- android-core/src/main/java/com/mparticle/internal/UserAudiencesRetriever.kt
+++ android-core/src/main/java/com/mparticle/internal/UserAudiencesRetriever.kt
@@ -7,9 +7,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-
 internal class UserAudiencesRetriever(private val apiClient: MParticleApiClient) {
-
 
     private val mApiClient: MParticleApiClient = apiClient
     fun fetchAudience(): AudienceTask<AudienceResponse> {
@@ -17,6 +15,6 @@ internal class UserAudiencesRetriever(private val apiClient: MParticleApiClient)
         CoroutineScope(Dispatchers.IO).launch {
             mApiClient.fetchUserAudience(task)
         }
-        return task;
+        return task
     }
 }
--- android-core/src/test/kotlin/com/mparticle/identity/audience/AudienceResponseTest.kt
+++ android-core/src/test/kotlin/com/mparticle/identity/audience/AudienceResponseTest.kt
@@ -7,34 +7,35 @@ import org.junit.Test
 import java.lang.reflect.Field
 import java.lang.reflect.Method
 
-
 class AudienceResponseTest {
 
-    private lateinit var classInstance:AudienceResponse
+    private lateinit var classInstance: AudienceResponse
 
     @Before
-    fun before(){
-        classInstance=AudienceResponse(-1, JSONObject())
+    fun before() {
+        classInstance = AudienceResponse(-1, JSONObject())
     }
+
     @Test
     fun parseJsonObjectTest() {
         val sampleJson =
             "{'dt':'ar','id':'54335128-0b2c-4089-a36d-8b456890dfe8','ct':1713390288601,'audience_memberships':[{'audience_id':13775},{'audience_id':13775}]}"
         val method: Method = AudienceResponse::class.java.getDeclaredMethod(
             "parseJsonObject",
-            JSONObject::class.java,
+            JSONObject::class.java
         )
         method.isAccessible = true
         val jsonObject1 = JSONObject(sampleJson)
         method.invoke(classInstance, jsonObject1)
         Assert.assertEquals(2, classInstance.getAudienceResult().size)
     }
+
     @Test
     fun parseJsonObjectTest_whenJSONIsEmpty() {
         val sampleJson = "{}"
         val method: Method = AudienceResponse::class.java.getDeclaredMethod(
             "parseJsonObject",
-            JSONObject::class.java,
+            JSONObject::class.java
         )
         method.isAccessible = true
         val jsonObject1 = JSONObject(sampleJson)
@@ -48,7 +49,7 @@ class AudienceResponseTest {
         classInstance = AudienceResponse(-1, JSONObject())
         val method: Method = AudienceResponse::class.java.getDeclaredMethod(
             "parseJsonObject",
-            JSONObject::class.java,
+            JSONObject::class.java
         )
         method.isAccessible = true
         val jsonObject1 = null
@@ -58,51 +59,52 @@ class AudienceResponseTest {
     }
 
     @Test
-    fun parseJsonObjectTest_when_AudienceMemberships_Field_Is_Not_Available(){
-        val sampleJson="{'dt':'ar','id':'54335128-0b2c-4089-a36d-8b456890dfe8','ct':1713390288601}"
+    fun parseJsonObjectTest_when_AudienceMemberships_Field_Is_Not_Available() {
+        val sampleJson =
+            "{'dt':'ar','id':'54335128-0b2c-4089-a36d-8b456890dfe8','ct':1713390288601}"
         val method: Method = AudienceResponse::class.java.getDeclaredMethod(
             "parseJsonObject",
-            JSONObject::class.java,
+            JSONObject::class.java
         )
         method.isAccessible = true
         val jsonObject1 = JSONObject(sampleJson)
         method.invoke(classInstance, jsonObject1)
 
-        Assert.assertEquals( 0,classInstance.getAudienceResult().size)
+        Assert.assertEquals(0, classInstance.getAudienceResult().size)
     }
 
     @Test
-    fun parseJsonObjectTest_when_Aaudience_id_Field_Is_Not_Available(){
-        val sampleJson="{'dt':'ar','id':'54335128-0b2c-4089-a36d-8b456890dfe8','ct':1713390288601,'audience_memberships':[{}]}"
+    fun parseJsonObjectTest_when_Aaudience_id_Field_Is_Not_Available() {
+        val sampleJson =
+            "{'dt':'ar','id':'54335128-0b2c-4089-a36d-8b456890dfe8','ct':1713390288601,'audience_memberships':[{}]}"
         val method: Method = AudienceResponse::class.java.getDeclaredMethod(
             "parseJsonObject",
-            JSONObject::class.java,
+            JSONObject::class.java
         )
         method.isAccessible = true
         val jsonObject1 = JSONObject(sampleJson)
         method.invoke(classInstance, jsonObject1)
 
-        Assert.assertEquals( 0,classInstance.getAudienceResult().size)
+        Assert.assertEquals(0, classInstance.getAudienceResult().size)
     }
 
     @Test
-    fun getErrorTest(){
-        val error_Msg="Unauthorized"
-        classInstance = AudienceResponse(401,error_Msg)
+    fun getErrorTest() {
+        val error_Msg = "Unauthorized"
+        classInstance = AudienceResponse(401, error_Msg)
         val field: Field = AudienceResponse::class.java.getDeclaredField("code")
-        field.isAccessible=true
-        Assert.assertEquals( error_Msg,classInstance.getError())
-        Assert.assertEquals( 401,field.getInt(classInstance))
-
+        field.isAccessible = true
+        Assert.assertEquals(error_Msg, classInstance.getError())
+        Assert.assertEquals(401, field.getInt(classInstance))
     }
 
     @Test
-    fun getErrorTest_When_Error_Is_Empty(){
-        val error_Msg=""
-        classInstance = AudienceResponse(-1,error_Msg)
+    fun getErrorTest_When_Error_Is_Empty() {
+        val error_Msg = ""
+        classInstance = AudienceResponse(-1, error_Msg)
         val field: Field = AudienceResponse::class.java.getDeclaredField("code")
-        field.isAccessible=true
-        Assert.assertEquals( error_Msg,classInstance.getError())
-        Assert.assertEquals( -1,field.getInt(classInstance))
+        field.isAccessible = true
+        Assert.assertEquals(error_Msg, classInstance.getError())
+        Assert.assertEquals(-1, field.getInt(classInstance))
     }
 }
