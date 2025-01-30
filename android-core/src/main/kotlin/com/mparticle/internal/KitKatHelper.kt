6ce2e7d08642e3b64c2c3251a44aca5967975d66
Mansi-mParticle <159845845+Mansi-mParticle@users.noreply.github.com>
2025-01-30 11:14:44 -0500

    refactor: Migrate Internal OS Helper class to kotlin (#515) (#536)

--- android-core/src/main/kotlin/com/mparticle/internal/KitKatHelper.kt
+++ android-core/src/main/kotlin/com/mparticle/internal/KitKatHelper.kt
@@ -1,19 +1,19 @@
-package com.mparticle.internal;
+package com.mparticle.internal
 
-import android.annotation.TargetApi;
-import android.os.Build;
-
-import org.json.JSONArray;
+import android.annotation.TargetApi
+import android.os.Build
+import org.json.JSONArray
 
 /**
  * This is solely used to avoid logcat warnings that Android will generate when loading a class,
  * even if you use conditional execution based on VERSION.
  */
 @TargetApi(19)
-public class KitKatHelper {
-    public static void remove(JSONArray array, int index) {
+object KitKatHelper {
+    @JvmStatic
+    fun remove(array: JSONArray, index: Int) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            array.remove(index);
+            array.remove(index)
         }
     }
 }
