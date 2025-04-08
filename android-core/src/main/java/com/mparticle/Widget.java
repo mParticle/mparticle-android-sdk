ca4f4e179cf75c58452d8cdc91d051631da6d51c
Mansi Pandya <mpandya@mparticle.com>
2025-04-07 23:34:11 -0400

    Removed unused variables

--- android-core/src/main/java/com/mparticle/Widget.java
+++ android-core/src/main/java/com/mparticle/Widget.java
@@ -6,9 +6,6 @@ import android.widget.FrameLayout;
 
 public class Widget extends FrameLayout {
 
-    private static final int OUT_OF_SYNC_HEIGHT_DIFF = 1;
-    private final int lastHeight = 0;
-
     public Widget(Context context) {
         super(context);
     }
