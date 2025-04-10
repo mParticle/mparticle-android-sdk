b3dea8fca682ec0aead796804d02e686c38df8e8
Mansi Pandya <mpandya@mparticle.com>
2025-04-10 15:08:05 -0400

    feat: Add sandbox mode to attributes

--- android-kit-base/src/main/java/com/mparticle/kits/KitManagerImpl.java
+++ android-kit-base/src/main/java/com/mparticle/kits/KitManagerImpl.java
@@ -38,6 +38,7 @@ import com.mparticle.identity.IdentityStateListener;
 import com.mparticle.identity.MParticleUser;
 import com.mparticle.identity.TaskFailureListener;
 import com.mparticle.identity.TaskSuccessListener;
+import com.mparticle.internal.Constants;
 import com.mparticle.internal.CoreCallbacks;
 import com.mparticle.internal.KitManager;
 import com.mparticle.internal.KitsLoadedCallback;
@@ -60,6 +61,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -1369,6 +1371,12 @@ public class KitManagerImpl implements KitManager, AttributionListener, UserAttr
                         objectAttributes.put(entry.getKey(), entry.getValue());
                     }
                     user.setUserAttributes(objectAttributes);
+
+                    if (!attributes.containsKey(Constants.MessageKey.SANDBOX_MODE_ROKT)) {
+                        attributes.put(Constants.MessageKey.SANDBOX_MODE_ROKT, String.valueOf(
+                                Objects.toString(MPUtility.isDevEnv(), "false")));  // Default value is "false" if null:wq
+                    }
+
                     ((KitIntegration.RoktListener) provider).execute(viewName,
                             attributes,
                             mpRoktEventCallback,
