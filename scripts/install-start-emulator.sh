2e71d18621079cf9c719b3ea5be3124f5938dad3
Mansi Pandya <mpandya@mparticle.com>
2025-09-17 16:42:38 -0400

    ignore emulator script in lint checks

--- scripts/install-start-emulator.sh
+++ scripts/install-start-emulator.sh
@@ -15,16 +15,16 @@
 
 # If there is an emulator already started, just use that one
 
-echo "run: started=$(adb get-state 1 || true)"
-started=$(adb get-state 1 || true)
-if [[ ${started} == "device" ]]; then
-	exit 0
+echo "run: started=$(adb get-state 1)"
+started=$(adb get-state 1)
+if [ "$started" = "device" ]; then
+  exit 0
 fi
 
 # Set PATH variable so we can find sdkmanager and avdmanager
 echo "Setting Path (emulator before tools hack)"
-export ANDROID_SDK="${HOME}/Library/Android/sdk"
-export PATH="${ANDROID_SDK}/emulator:${ANDROID_SDK}/tools/:${ANDROID_HOME}/tools/bin:${PATH}"
+export ANDROID_SDK=$HOME/Library/Android/sdk
+export PATH=$ANDROID_SDK/emulator:$ANDROID_SDK/tools/:$ANDROID_HOME/tools/bin:$PATH
 #echo "run: sdkmanager tools"
 #sdkmanager tools
 #echo "run: kill-server"
@@ -37,11 +37,11 @@ export PATH="${ANDROID_SDK}/emulator:${ANDROID_SDK}/tools/:${ANDROID_HOME}/tools
 #echo "run: adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done &"
 #adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done &
 echo "starting emulator"
-echo 'run: echo yes | sdkmanager --install "system-images;android-28;default;x86"'
+echo "run: echo yes | sdkmanager --install \"system-images;android-28;default;x86\""
 echo yes | sdkmanager --install "system-images;android-29;default;x86"
 
 # create new Emulator
-echo 'run: echo no | avdmanager create avd --force -n test -k "system-images;android-29;default;x86" -c 10M'
+echo "run: echo no | avdmanager create avd --force -n test -k \"system-images;android-29;default;x86\" -c 10M"
 echo no | avdmanager create avd --force -n test -k "system-images;android-29;default;x86" -c 10M
 
 # sanity check, make sure new Emulator was created
@@ -49,22 +49,22 @@ echo "run: emulator -list-avds"
 emulator -list-avds
 
 # start Emulator (not sure if we actually have to run this as sudo or not
-echo "run: sudo -E sudo -u ${USER} -E bash -c \"./emulator -avd test -verbose -no-snapshot -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &\""
-sudo -E sudo -u "${USER}" -E bash -c "${ANDROID_HOME}/emulator/emulator -avd test -verbose -no-snapshot -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &"
+echo "run: sudo -E sudo -u $USER -E bash -c \"./emulator -avd test -verbose -no-snapshot -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &\""
+sudo -E sudo -u "$USER" -E bash -c "${ANDROID_HOME}/emulator/emulator -avd test -verbose -no-snapshot -no-window -camera-back none -camera-front none -selinux permissive -qemu -m 2048 &"
 
 echo "run: adb shell input keyevent 82 &"
 adb shell input keyevent 82 &
 
 #block until emulator is ready to run tests
 echo "Waiting for Emulator to start"
-until [[ ${bootanim} =~ "stopped" ]]; do
-	echo "run: adb reconnect offline"
-	adb reconnect offline
-	adb devices
-	bootanim="$(adb -e shell getprop init.svc.bootanim 2>&1)"
-	echo "Waiting for emulator, status is: ${bootanim}"
-	sleep 1
-	echo
+until [[ "$bootanim" =~ "stopped" ]]; do
+  echo "run: adb reconnect offline"
+  adb reconnect offline
+  adb devices
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  echo "Waiting for emulator, status is: $bootanim"
+  sleep 1
+  echo
 done
 
 echo "Emulator has started - dismissing keyboard and sleeping for 1 second."
