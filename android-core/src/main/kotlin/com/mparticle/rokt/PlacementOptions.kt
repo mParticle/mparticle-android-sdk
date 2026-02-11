39a961f697d8bfd6e29032e0d996508002490cab
Thomson Thomas <thomson.thomas@rokt.com>
2026-02-11 13:06:07 -0500

    Make the dynamicPerformanceMarkers immutable

--- android-core/src/main/kotlin/com/mparticle/rokt/PlacementOptions.kt
+++ android-core/src/main/kotlin/com/mparticle/rokt/PlacementOptions.kt
@@ -1,3 +1,3 @@
 package com.mparticle.rokt
 
-data class PlacementOptions(val jointSdkSelectPlacements: Long, val dynamicPerformanceMarkers: MutableMap<String, Long> = mutableMapOf())
+data class PlacementOptions(val jointSdkSelectPlacements: Long, val dynamicPerformanceMarkers: Map<String, Long> = mapOf())
